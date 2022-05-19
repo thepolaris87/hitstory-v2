2f66962df3869629d75c1d5b5aefaa89896a3d4e
김진범 <polaris@esls.io>
2022-05-20 08:34:22 +0900

    update

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
-
 export default function Router() {
     return (
         <BrowserRouter>
-            <Routes>                
+            <Routes>
+                <Route path='*' element={<Navigate replace to='/'></Navigate>}></Route>
                 <Route path='*' element={<Navigate replace to='/'></Navigate>}></Route>
             </Routes>
         </BrowserRouter>
